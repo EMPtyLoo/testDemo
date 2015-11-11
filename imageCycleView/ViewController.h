31ae96eeb51d67306b2558ca0fce4cd69960749b
EMPtyLoo <457607792@qq.com>
2015-11-11 21:14:07 +0800

    Initial Commit

---
+++ imageCycleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  imageCycleView
+//
+//  Created by EMPty on 11/11/15.
+//  Copyright (c) 2015 EMPty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ imageCycleView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  imageCycleView
+//
+//  Created by EMPty on 11/11/15.
+//  Copyright (c) 2015 EMPty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
