e9eccf8400c021e8c7d373486c50006bc4f9bf43
EMPtyLoo <457607792@qq.com>
2015-12-23 11:47:35 +0800

    first test demo

---
+++ imageCycleView/scrollView.h
@@ -0,0 +1,13 @@
+//
+//  scrollView.h
+//  imageCycleView
+//
+//  Created by EMPty on 11/11/15.
+//  Copyright (c) 2015 EMPty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface scrollView : UIScrollView
+
+@end
---
+++ imageCycleView/scrollView.m
@@ -0,0 +1,21 @@
+//
+//  scrollView.m
+//  imageCycleView
+//
+//  Created by EMPty on 11/11/15.
+//  Copyright (c) 2015 EMPty. All rights reserved.
+//
+
+#import "scrollView.h"
+
+@implementation scrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
